81a4f15264adc53c726888003817274d19530bf7
Lzsofiae <miliczkij@gmail.com>
2022-11-16 23:28:31 +0100

    Problem 4

---
+++ Problem4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem3{
+  class Program{
+    static void Main(string [] args){
+      List<long> ps = new List<long>();
+      for (int i=100; i<1000; i++){
+        for (int j=100; j<1000; j++){
+          if(palindrome(i*j)){
+            Console.WriteLine(i + " * " + j + " = " + i*j);
+            ps.Add(i*j);
+          }
+        }
+      }
+      ps.Sort();
+      Console.WriteLine(ps.Last());
+    }
+    static bool palindrome(long n){
+      string comp1 = n.ToString();
+      string comp2 = Reverse(n.ToString());
+      return String.Equals(comp1, comp2);
+    }
+    public static string Reverse(string s){
+      char[] charArray = s.ToCharArray();
+      Array.Reverse(charArray);
+      return new string(charArray);
+    }
+  }
+}
