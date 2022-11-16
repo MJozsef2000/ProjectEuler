9fdc281f60a1f097bc801e0d2d9e861b01902501
Lzsofiae <miliczkij@gmail.com>
2022-11-16 23:50:47 +0100

    Problem6

---
+++ Problem6/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Problem6{
+  class Program{
+    static void Main(string [] args){
+      double sum1 = 0;
+      double sum2 = 0;
+      for(int i=1; i<101; i++){
+        sum1 += Math.Pow(Convert.ToDouble(i),2.0);
+        sum2 += i;
+      }
+      sum2 = Math.Pow(Convert.ToDouble(sum2),2.0);
+      Console.WriteLine(sum2-sum1);
+    }
+  }
+}
