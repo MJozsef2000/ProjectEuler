661e620b14ab86eb721f3f646d274a6cd8536090
Lzsofiae <miliczkij@gmail.com>
2022-11-16 23:34:54 +0100

    Problem 5

--- Problem5/Program.cs
+++ Problem5/Program.cs
@@ -1,6 +1,18 @@
 ﻿namespace Problem5{
   class Program{
     static void Main(string [] args){
+      int i=1;
+      while(check(i) == false)
+        i++;
+      Console.WriteLine("Result: " + i);
+    }
+
+    static bool check(long n){
+      for (int i=1; i<21; i++){
+        if (n%i != 0)
+          return false;
+      }
+      return true;
     }
   }
 }
