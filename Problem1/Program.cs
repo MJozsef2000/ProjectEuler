782c3d4092c6c1f1eee81d5d8e5e9bb2421e2766
MJozsef2000 <78852131+MJozsef2000@users.noreply.github.com>
2022-11-16 21:08:12 +0100

    Add files via upload

---
+++ Problem1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int sum = 0;
+          for (int i=0; i<1000; i++){
+            if (i % 3 == 0 || i % 5 == 0)
+              sum += i;
+          }
+          Console.WriteLine("Sum of multiples: " + sum);
+        }
+    }
+}
